54639e48c3f2c5c81f18bdbd530eb4b425d403f5
frixtar <frix473@gmail.com>
2026-05-10 23:56:52 -0600

    sistema de inventario MERN completo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://localhost:5001/api';
 const api = axios.create({ baseURL: API_URL, headers: { 'Content-Type': 'application/json' } });
 
 export const inventarioService = {
